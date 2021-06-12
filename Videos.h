32cdca62239d72be46f74e1c3649b0f7bcc5a812
Roberto A. Perez | Skynet_Mitnick <80007188+SkynetMitnick@users.noreply.github.com>
2021-06-11 20:44:29 -0500

    Add files via upload

--- Videos.h
+++ Videos.h
@@ -1,9 +1,21 @@
 #ifndef VIDEOS_H
 #define VIDEOS_H
 
+#include <iostream>
+
+using namespace std;
+
 class Videos
 {
+  public:
+    int id;
+    string nombre;
+    double duracion;
+    string genero;
 
+  public:
+    void videos();
+    void videos(int,string,double,string);
 };
 
 #endif
--- Videoteca.h
+++ Videoteca.h
@@ -1,9 +1,19 @@
 #ifndef VIDEOTECA_H
 #define VIDEOTECA_H
 
-class Videoteca
-{
+#include "Videos.h"
 
+class Videoteca : Videos
+{
+  public:
+    int cantidadVideos;
+    Videos coleccion;
+  
+  public:
+    void videoteca();
+    void videoteca(int);
+    void show();
+    void set_video(int,Videos);
 };
 
 #endif
